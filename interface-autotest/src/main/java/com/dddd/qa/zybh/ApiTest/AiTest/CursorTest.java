cb024987f46b6976299e7180643cc7c6886cda88
ZhangSCzybh <17858803001@163.com>
2025-03-17 15:49:13 +0800

    CursorTest

---
+++ interface-autotest/src/main/java/com/dddd/qa/zybh/ApiTest/AiTest/CursorTest.java
@@ -0,0 +1,11 @@
+package com.dddd.qa.zybh.ApiTest.AiTest;
+
+/**
+ * @author zhangsc
+ * @date 2025年03月17日 15:48:43
+ * @packageName com.dddd.qa.zybh.ApiTest.AiTest
+ * @className CursorTest
+ * @describe TODO
+ */
+public class CursorTest {
+}
